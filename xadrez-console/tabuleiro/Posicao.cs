acb24f607bfc9f3d9ff15659719c8db75b4c8f7b
Guilherme <gui100920@gmail.com>
2024-03-31 18:44:09 -0300

    Criando a classe posicao

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+	class Posicao
+	{
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+
+    }
+}
